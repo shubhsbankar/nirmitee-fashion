a2cd795a0e135a976e395d995106f1528a2f11b9
Shubham Bankar <shubhs.bankar@gmail.com>
2025-12-10 18:06:20 +0530

    Started wesite panel dev

--- app/(root)/(website)/layout.tsx
+++ app/(root)/(website)/layout.tsx
@@ -2,14 +2,14 @@ import Footer from "@/components/application/website/Footer";
 import Header from "@/components/application/website/Header";
 import { Kumbh_Sans } from "next/font/google";
 
-const kumbh = Kumbh_Sans({ subsets: ["latin"], weight: ["400", "500", "600","700","800"] , display: "swap" });
 
+const kumbh = Kumbh_Sans({ subsets: ["latin"], weight: ["400","500" ,"600","700","800"], display: "swap" });
 const layout = ({ children }) => {
     return (
         <div className={kumbh.className}>
-            <Header />
-            <main>{children}</main>
-            <Footer />
+        <Header />
+        <main>{children}</main>
+        <Footer />
         </div>
     );
 };
