7302925ad91c6300e280211406114bcf02afa4eb
Shubham Bankar <60430028+shubhsbankar@users.noreply.github.com>
2025-12-24 21:53:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+const nextConfig =   {
+  images:{ 
+    remotePatterns:[{
+      protocol: "https",
+      hostname: "res.cloudinary.com",
+      port:"",
+      pathname:"/**",
+      search:""
+      }]
+      }
+      };
+export default nextConfig;
 module.exports = {
   typescript: {
     ignoreBuildErrors: true,
