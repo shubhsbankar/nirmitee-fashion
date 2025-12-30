f3b0e53753a11f23adf5c255287ee095f5ace1e6
Shubham Bankar <shubhsbankar>
2025-12-30 18:57:03 +0530

    Added error log

--- app/api/dashboard/admin/latest-review/route.js
+++ app/api/dashboard/admin/latest-review/route.js
@@ -27,6 +27,7 @@ export async function GET(request) {
         return response(true, 200, 'Latest reviews.', latestReviews);
 
     } catch (error) {
+        console.error(error);
         return catchError(error);
     }
 }
